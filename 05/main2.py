0ff0af5dea559de1ac6159a97e94d2c24fe70f19
petr.cech.ext <petr.cech2@bizztreat.com>
2023-12-06 08:40:59 +0100

    05

---
+++ 05/main2.py
@@ -0,0 +1,55 @@
+#not working yet
+def seeds_sum(seeds):
+    print('Counting Seeds')
+    origin_seeds = seeds[::2]
+    seeds_multiplier = seeds[1::2]
+    new_seed_list = []
+    for i in range(len(origin_seeds)):
+        for j in range(int(seeds_multiplier[i])):
+            new_seed_list.append(int(origin_seeds[i])+j)
+    return new_seed_list
+
+def counter(mapping,seeds):
+    print('Running Mapping Counter')
+    #print(f'running encounter: {mapping}')
+    if not mapping:
+        return seeds
+    else:
+        flag_list = [None] * len(seeds)
+        for item in mapping:
+            destination_num = int(item[0])
+            source_num = int(item[1])
+            range_len = int(item[2])
+            for seed_position, seed in enumerate(seeds):
+                if int(source_num) <= int(seed) < int(source_num)+range_len and flag_list[seed_position] != 'CH':
+                    #print(f"Seed je: {seed}, source_num je {source_num} a destination_num je {destination_num}")
+                    seeds[seed_position] = int(seed) - int(source_num) + int(destination_num)
+                    flag_list[seed_position] = 'CH' 
+        #print(f"Seeds = {seeds}")
+        return seeds
+
+
+def main():
+    with open('input.txt') as f:
+        input = f.read().split('\n')
+        last_line = input[-1]
+        mapping = []
+        for row in input:
+            if row.strip():
+                #print(f"Row: {row}")
+                if row.find('seeds') != -1:
+                    seeds = seeds_sum(row.split(': ')[1].split(' '))
+                elif row.find('map') != -1:
+                    seeds = counter(mapping,seeds)
+                    mapping = []
+                elif str(row)[0].isnumeric():
+                    mapping.append(row.split(' '))
+                
+                if row==last_line:
+                    seeds = counter(mapping,seeds)
+        #print(seeds)
+        seeds.sort()
+        print(seeds[0])
+
+if __name__ == "__main__":
+    main()
